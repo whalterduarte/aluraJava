9c8e442920aba2e0094cf57a4d3193c4badc2521
Whalter Duarte <whalterdev@gmail.com>
2024-03-21 16:54:28 -0300

    classes e objeto

---
+++ orientacaoobjeto/src/Principal.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "Naruto";
+        System.out.println("Filme preferido" + meuFilme.nome);
+    }
+}
